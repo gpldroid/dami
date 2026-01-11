6e3e6ffd02bdc3f07ee1ac35fc6f4a0433eef88c
imad limrani <148809071+gpldroid@users.noreply.github.com>
2026-01-11 19:05:06 +0100

    Update api_proxy.php

--- api_proxy.php
+++ api_proxy.php
@@ -1,15 +1,29 @@
 <?php
-// api_proxy.php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$url = "http://ottpro.iptvpro2.com:8789/player_api.php?" . $_SERVER['QUERY_STRING'];
+// بيانات السيرفر (محمية داخل PHP لا يراها المستخدم)
+$base_url = "http://ottpro.iptvpro2.com:8789/player_api.php";
+$username = "Oujakr12";
+$password = "87593226";
 
-// استخدام cURL لجلب البيانات من سيرفر IPTV
+// بناء الرابط بناءً على الطلبات القادمة من الـ JS
+$query = http_build_query([
+    'username' => $username,
+    'password' => $password,
+    'action'   => $_GET['action'] ?? '',
+    'category_id' => $_GET['category_id'] ?? '',
+    'series_id'   => $_GET['series_id'] ?? '',
+    'vod_id'      => $_GET['vod_id'] ?? ''
+]);
+
+$final_url = $base_url . "?" . $query;
+
+// جلب البيانات
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_URL, $final_url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_TIMEOUT, 30);
 $response = curl_exec($ch);
 curl_close($ch);
 
